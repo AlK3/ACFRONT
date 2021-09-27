12fff4848e992c18ff2299bb9cac4829da7d12ec
AlK3 <alsenpaymoon@gmail.com>
2021-09-27 18:11:13 +1000

    update commit

--- src/pages/AnimePage.jsx
+++ src/pages/AnimePage.jsx
@@ -6,19 +6,13 @@ import { PaginatorTypeTwo } from "../components/Paginator/PaginatorTypeTwo";
 import QuoteList from "../components/QuoteList/QuoteList";
 
 export const AnimePage = (props) => {
-    const dispatch = useDispatch();
     const load = useSelector(state => state.load.load);
     const quotes = useSelector(state => state.quote.quotes);
-
-    console.log("QQQQQQQQQQQQ");
 
     return (
         <>
             {load ? <Loader /> : <QuoteList quotes={quotes} />}
             {quotes[0] && <PaginatorTypeTwo />}
-            <div className='wrapper'>
-                <Button color='#faa' onClick={() => dispatch({type: 'UPDATE_PAGE_ANIME', payload: {anime: '', page: 0}})}>Back</Button>
-            </div>
         </>
     );
 }
